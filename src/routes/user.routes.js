aae37eb758d6dce1650a3022bcb5c8bfa512a0ac
Armaan-Ali-786 <armaan2785@gmail.com>
2026-03-26 12:49:56 +0530

    add register controller

---
+++ src/routes/user.routes.js
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+
+const router = Router();
+
+router.route('/register').post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1 // can take more if need
+        }
+    ]),
+    registerUser
+);
+
+export default router;
