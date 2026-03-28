a2c8ce305c40c2afc5ea3fdd673dd247a4c0bac5
Armaan-Ali-786 <armaan2785@gmail.com>
2026-03-28 14:15:14 +0530

    add channel profile and watch history features

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser, loginUser, logOutUser, refreshAccessToken } from "../controllers/user.controller.js";
+import { registerUser, loginUser, logOutUser, refreshAccessToken, channelProfile } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -25,5 +25,7 @@ router.route('/login').post(loginUser);
 
 router.route('/logout').post(verifyJWT, logOutUser);
 router.route('/refresh-token').post(refreshAccessToken);
+
+router.route('/channeldetail', channelProfile);
 
 export default router;
